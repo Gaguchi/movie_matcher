24242e13ea08d4dfb879c2b3edb09c45d50ffb08
Gaguchi <gaga9393@gmail.com>
2023-11-10 20:57:01 +0400

    small bug when displaying the winner title

--- movie_match/static/movie_match/assets/js/wheel.js
+++ movie_match/static/movie_match/assets/js/wheel.js
@@ -1,25 +1,32 @@
 var imageScale = 0.9;
 var imagePosition = 100;
 var wheelRotationAngle = 0; // Global rotation angle for the wheel
+var isSpinning = false;
+
+// Preloaded images and their state
+var wheelImages = [];
+
+// Function to preload images for the wheel
+function loadWheelImages(movies, callback) {
+    var loadedCount = 0;
+    movies.forEach(function(movie, index) {
+        var image = new Image();
+        image.onload = function() {
+            loadedCount++;
+            if (loadedCount === movies.length) {
+                callback(); // Call drawWheel when all images are loaded
+            }
+        };
+        image.src = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2' + movie.fields.poster_path;
+        wheelImages.push({ image: image, movie: movie });
+    });
+    console.log(movies)
+}
 
 // Function to draw the wheel with movies
 function drawWheel(canvas, rotationAngle) {
     var ctx = canvas.getContext('2d');
-    var moviesElement = document.getElementById('movies-data');
-
-    if (!moviesElement) {
-        console.error('Movies data element not found');
-        return;
-    }
-
-    var movies = JSON.parse(moviesElement.textContent);
-
-    if (!Array.isArray(movies)) {
-        console.error('Invalid movies data:', movies);
-        return;
-    }
-
-    var numberOfSegments = Math.min(movies.length, 8);
+    var numberOfSegments = Math.min(wheelImages.length, 8);
     var angle = rotationAngle * Math.PI / 180;
     var arc = Math.PI * 2 / numberOfSegments;
     var outsideRadius = canvas.width / 2 - 20;
@@ -27,10 +34,11 @@ function drawWheel(canvas, rotationAngle) {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    movies.forEach(function(movie, index) {
+    wheelImages.forEach(function(wheelImage, index) {
         var angleStart = angle + index * arc;
         var angleEnd = angleStart + arc;
 
+        // Draw segment
         ctx.beginPath();
         ctx.arc(canvas.width / 2, canvas.height / 2, outsideRadius, angleStart, angleEnd, false);
         ctx.arc(canvas.width / 2, canvas.height / 2, insideRadius, angleEnd, angleStart, true);
@@ -38,29 +46,26 @@ function drawWheel(canvas, rotationAngle) {
         ctx.fillStyle = 'hsl(' + (index * 360 / numberOfSegments) + ', 100%, 50%)';
         ctx.fill();
 
-        var image = new Image();
-        image.onload = function() {
-            ctx.save();
-            ctx.beginPath();
-            ctx.arc(canvas.width / 2, canvas.height / 2, outsideRadius, angleStart, angleEnd, false);
-            ctx.arc(canvas.width / 2, canvas.height / 2, insideRadius, angleEnd, angleStart, true);
-            ctx.closePath();
-            ctx.clip();
+        // Draw image
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2, canvas.height / 2, outsideRadius, angleStart, angleEnd, false);
+        ctx.arc(canvas.width / 2, canvas.height / 2, insideRadius, angleEnd, angleStart, true);
+        ctx.closePath();
+        ctx.clip();
 
-            var scale = Math.min((outsideRadius - insideRadius) / image.width, (outsideRadius - insideRadius) / image.height);
-            scale *= imageScale; // Apply the scale from the variable
-            var scaledWidth = scale * image.width;
-            var scaledHeight = scale * image.height;
+        var scale = Math.min((outsideRadius - insideRadius) / wheelImage.image.width, (outsideRadius - insideRadius) / wheelImage.image.height);
+        scale *= imageScale; // Apply the scale from the variable
+        var scaledWidth = scale * wheelImage.image.width;
+        var scaledHeight = scale * wheelImage.image.height;
 
-            var imageX = canvas.width / 2 + Math.cos(angleStart + arc / 2) * (insideRadius + imagePosition);
-            var imageY = canvas.height / 2 + Math.sin(angleStart + arc / 2) * (insideRadius + imagePosition);
+        var imageX = canvas.width / 2 + Math.cos(angleStart + arc / 2) * (insideRadius + imagePosition);
+        var imageY = canvas.height / 2 + Math.sin(angleStart + arc / 2) * (insideRadius + imagePosition);
 
-            ctx.translate(imageX, imageY);
-            ctx.rotate(angleStart + arc / 2 + Math.PI / 2);
-            ctx.drawImage(image, -scaledWidth / 2, -scaledHeight / 2, scaledWidth, scaledHeight);
-            ctx.restore();
-        };
-        image.src = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2' + movie.fields.poster_path;
+        ctx.translate(imageX, imageY);
+        ctx.rotate(angleStart + arc / 2 + Math.PI / 2);
+        ctx.drawImage(wheelImage.image, -scaledWidth / 2, -scaledHeight / 2, scaledWidth, scaledHeight);
+        ctx.restore();
     });
 
     drawCenterButton(canvas);
@@ -98,8 +103,20 @@ function drawCenterButton(canvas) {
 
 // Function to spin the wheel
 function spinWheel(canvas) {
-    var ctx = canvas.getContext('2d');
+    if (isSpinning) {
+        return; // Prevent additional spins if the wheel is already spinning
+    }
 
+    isSpinning = true; // Set the flag to indicate the wheel is spinning
+
+    var moviesElement = document.getElementById('movies-data');
+    if (!moviesElement) {
+        console.error('Movies data element not found');
+        return;
+    }
+    var movies = JSON.parse(moviesElement.textContent);
+
+    var numberOfSegments = Math.min(movies.length, 8);
     var newAngle = wheelRotationAngle + Math.random() * 360 + 360 * 5;
     wheelRotationAngle = newAngle % 360;
 
@@ -109,26 +126,30 @@ function spinWheel(canvas) {
         drawWheel(canvas, currentAngle);
         if (Math.abs(newAngle - currentAngle) < 0.5) {
             clearInterval(interval);
+            isSpinning = false; // Reset the flag once the spin is complete
+            // Determine the winning movie
+            var winningIndex = Math.floor(((270 - (newAngle % 360)) / 360) * numberOfSegments) % numberOfSegments;
+            var winningMovie = movies[winningIndex];
+            console.log('Winning movie:', winningMovie.fields.title); // Log the title of the winning movie
         }
     }, 16);
 }
 
-// Event listener for the wheel
-document.getElementById('movieWheelCanvas').addEventListener('click', function() {
-    var canvas = document.getElementById('movieWheelCanvas');
-    spinWheel(canvas);
-});
 
-// Initial setup to draw static parts of the wheel
+// Initial setup and image loading
 window.onload = function() {
     var canvas = document.getElementById('movieWheelCanvas');
     if (canvas) {
-        drawCenterButton(canvas);
-        drawIndicator(canvas);
-        drawWheel(canvas, 0); // Draw initial wheel
+        var moviesElement = document.getElementById('movies-data');
+        if (moviesElement) {
+            var movies = JSON.parse(moviesElement.textContent);
+            loadWheelImages(movies, function() {
+                drawWheel(canvas, 0); // Draw initial wheel
+            });
+        } else {
+            console.error('Movies data element not found');
+        }
     } else {
         console.error('Canvas element not found');
     }
 };
-
-// ... [Any additional code or event listeners for sliders]
