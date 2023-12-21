c0a06682165e05a55283b29fb46742c9f447b4aa
Gaguchi <gaga9393@gmail.com>
2023-12-21 22:37:04 +0400

    Update Plinko game canvas size

--- movie_match/static/movie_match/assets/js/plinko.js
+++ movie_match/static/movie_match/assets/js/plinko.js
@@ -1,3 +1,4 @@
+
 class Peg {
     constructor(x, y) {
         this.x = x;
@@ -17,7 +18,7 @@ class Ball {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.radius = 10;
+        this.radius = 5;
         this.vx = 0;
         this.vy = 0;
         this.gravity = 0.2;  // Increase gravity
@@ -59,14 +60,37 @@ class Plinko {
         this.ctx = canvas.getContext('2d');
         this.pegs = [];
         this.balls = [];
+        this.sections = 8;  // Number of sections
         this.createPegs();
     }
 
+    drawWalls() {
+        const lineY = this.canvas.height - 50;
+        const sectionWidth = this.canvas.width / this.sections;
+
+        this.ctx.fillStyle = '#000';  // Set the fill color to black
+        this.ctx.textBaseline = 'middle';
+        this.ctx.font = '20px Arial';
+
+        for (let i = 0; i <= this.sections; i++) {
+            // Draw the line
+            this.ctx.beginPath();
+            this.ctx.moveTo(i * sectionWidth, lineY);
+            this.ctx.lineTo(i * sectionWidth, this.canvas.height);
+            this.ctx.stroke();
+
+            // Draw the number
+            if (i < this.sections) {
+                this.ctx.fillText(i + 1, i * sectionWidth + sectionWidth / 2, this.canvas.height - 25);
+            }
+        }
+    }
+
     createPegs() {
-        const rows = 10;
-        const cols = 7;
-        const spacing = 50;
-        const offsetX = 10;  // Add an offset to the x position of the pegs
+        const rows = 15;
+        const cols = 11;
+        const spacing = 30;
+        const offsetX = 0;  // Add an offset to the x position of the pegs
 
         for (let row = 0; row < rows; row++) {
             for (let col = 0; col < cols; col++) {
@@ -86,6 +110,9 @@ class Plinko {
     }
 
     update() {
+        const sectionWidth = this.canvas.width / this.sections;
+    
+
         for (const ball of this.balls) {
             ball.update();
 
@@ -125,12 +152,29 @@ class Plinko {
             if (ball.y + ball.radius > this.canvas.height) {
                 ball.bounce();  // Make the ball bounce
             }
+
+            // Check if the ball has reached the bottom of the canvas
+            if (ball.y + ball.radius > this.canvas.height - 50) {
+                // Calculate which section the ball is in
+                const section = Math.floor(ball.x / sectionWidth);
+
+                // Log the section number to the console
+                console.log('Ball in section:', section + 1);
+            }
+            // Check collision with lines
+            for (let i = 1; i < this.sections; i++) {
+                if (ball.y + ball.radius > this.canvas.height - 50 && ball.x > i * sectionWidth && ball.x < (i * sectionWidth + 10)) {
+                    ball.vx *= -1;
+                }
+            }
         }
     }
 
     draw() {
         console.log(`Drawing ${this.pegs.length} pegs and ${this.balls.length} balls`);  // Debugging log
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.drawWalls();  // Draw the walls
 
         for (const peg of this.pegs) {
             peg.draw(this.ctx);
