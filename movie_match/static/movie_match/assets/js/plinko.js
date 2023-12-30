9c5097b47c76b30f4a67cdea667e6cd3aa704969
Gaguchi <gaga9393@gmail.com>
2023-12-30 23:28:07 +0400

    Update movie profile link in Plinko modal

--- movie_match/static/movie_match/assets/js/plinko.js
+++ movie_match/static/movie_match/assets/js/plinko.js
@@ -288,6 +288,7 @@ class Plinko {
             modal.querySelector('.text-muted').textContent = winningMovie.fields.release_date;
             modal.querySelector('.modal-image').src = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2' + winningMovie.fields.poster_path;
             modal.querySelector('.modal-content').style.background = 'url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces' + winningMovie.fields.backdrop_path +')';
+            modal.querySelector('.movie-link').href = 'movie_match/movie_profile' + winningMovie.fields.id;
             var modalInstance = new bootstrap.Modal(modal);
             modalInstance.show();
         } else {
